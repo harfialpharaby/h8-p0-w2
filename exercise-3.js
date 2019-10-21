ddd27faf67b910b03e7438c16f3e21638e17b5aa
Harfi <harfialpharaby@gmail.com>
2019-10-21 21:54:35 +0700

    ubah kondisi jika nama kosong

--- exercise-3.js
+++ exercise-3.js
@@ -1,22 +1,21 @@
 // contoh input
 var nama = '';
-var peran = '';
+var peran = 'tes';
 var hasil;
 
-if(nama === '' && peran === '') {
+if(nama === '') {
     console.log('Nama harus diisi!');
 } else if(nama !== '' && peran === '') {
     console.log(`Halo ${nama}, pilih peranmu untuk memulai game!`);
 } else {
     console.log(`Selamat datang di dunia Proxytia, ${nama}`);
-    hasil = `Halo ${peran} ${nama},`;
     if (peran.toUpperCase() === 'KSATRIA') {
-        console.log(`${hasil} kamu dapat menyerang dengan senjatamu!`);
+        console.log(`Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`);
     } else if (peran.toUpperCase() === 'TABIB') {
-        console.log(`${hasil} kamu akan membantu temanmu yang terluka.`);
+        console.log(`Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka.`);
     } else if (peran.toUpperCase() === 'PENYIHIR') {
-        console.log(`${hasil} ciptakan keajaiban yang membantu kemenanganmu!`);
+        console.log(`Halo ${peran} ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
     } else {
-        console.log(`${hasil} maaf peran ini belum tersedia`); 
+        console.log(`Halo ${peran} ${nama}, maaf peran ini belum tersedia`); 
     }
 }
